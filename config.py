da72d77fe97c817d68d7ca177b27bbd73612e18f
Madara <147428837+Madara1188@users.noreply.github.com>
2025-07-25 20:31:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,9 +29,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5734659617")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://EternalsBots:EternalsBots@cluster0.heqn4vw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
-DB_NAME = os.environ.get("DATABASE_NAME", "EternalsBots")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Furina:Furina@cluster0.nqumuxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Furina")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Team_Dst")
