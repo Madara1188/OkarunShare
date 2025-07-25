d54f3fb7645f91532b31b12b9fb9d8201d46b336
Madara <147428837+Madara1188@users.noreply.github.com>
2025-07-25 22:55:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -279,13 +279,8 @@ async def not_joined(client: Client, message: Message):
                                 expire_date=datetime.utcnow() + timedelta(seconds=FSUB_LINK_EXPIRY) if FSUB_LINK_EXPIRY else None)
                             link = invite.invite_link
 
-                    buttons= ([
-        [
-            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ⚡️", url=link),
-            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ⚡️", url=link)
-        ]
-    ])
-                    count += 2
+                    buttons.append([InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ⚡️", url=link)])
+                    count += 1
                     await temp.edit(f"<b>{'! ' * count}</b>")
 
                 except Exception as e:
